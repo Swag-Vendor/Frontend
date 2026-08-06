de042b51258ed1d465166fd74155485d4c92907b
Sanya Gupta <88279411+sanyagup@users.noreply.github.com>
2026-08-06 12:13:17 -0400

    added delete quote files, nothing implemented yet

---
+++ src/components/DeleteQuote.tsx
@@ -0,0 +1,12 @@
+import { FormEvent, useState } from 'react';
+// @ts-ignore: allow CSS side-effect import without type declarations
+import './DeleteQuote.css';
+
+export default function DeleteQuote() {
+
+    return (
+        <div>
+            <h1>Delete Quote</h1>
+        </div>
+    );
+}
